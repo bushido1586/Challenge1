61587de78968d1b152a9971dffbbf3c7b5bfc070
Brad Reed <shenmusha@yahoo.com>
2021-01-03 20:46:07 -0500

    character search input (Challenge 2) solution

--- Program.cs
+++ Program.cs
@@ -6,18 +6,16 @@ namespace Challenge1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter your name and press enter:");
-            string name = Console.ReadLine();
-            string nameUpperCase = String.Format("Upper Case : {0}", name.ToUpper());
-            string nameLowerCase = String.Format("Lower Case : {0}", name.ToLower());
-            string nameTrimmed = String.Format("Trimmed value : {0}", name.Trim());
-            string nameSubString = String.Format("Substring value : {0}", name.Substring(0, 5));
+            Console.WriteLine("Enter a string here: ");
+            //reasd first input string
+            string entry = Console.ReadLine();
 
-            Console.WriteLine(nameUpperCase);
-            Console.WriteLine(nameLowerCase);
-            Console.WriteLine(nameTrimmed);
-            Console.WriteLine(nameSubString);
-
+            Console.WriteLine("Enter the character to seach: ");
+            //Read char input for search
+            char searchInput = Console.ReadLine()[0];
+            //Gets INdex of char from string
+            int searchIndex = entry.IndexOf(searchInput);
+            Console.WriteLine("Index of character {0} in string is {1}", searchInput, searchIndex);
         }
     }
 }
