f699b09a44c008649ed2cb83a5b93b9061b152c8
Brad Reed <shenmusha@yahoo.com>
2021-01-03 20:23:27 -0500

    Challenge 1 : String and its Methods (C#)

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace Challenge1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter your name and press enter:");
+            string name = Console.ReadLine();
+            Console.WriteLine(name.ToUpper());
+            Console.WriteLine(name.ToLower());
+            Console.WriteLine(name.Trim());
+            Console.WriteLine(name.Substring(1));
+
         }
     }
 }
