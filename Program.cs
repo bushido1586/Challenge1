23202ec1a927738d47f40986c773fbf1e9632369
Brad Reed <shenmusha@yahoo.com>
2021-01-12 16:32:41 -0500

    If challenge answer

--- Program.cs
+++ Program.cs
@@ -4,20 +4,26 @@ namespace Challenge1
 {
     class Program
     {
+        static int highscore = 300;
+        static string highScorePlayer = "Brad";
         static void Main(string[] args)
         {
-            string name1 = "Paul";
-            string name2 = "Holly";
-            string name3 = "Aaron";
-            GreetFriend(name1);
-            GreetFriend(name2);
-            GreetFriend(name3);
-        }
+            ScoreCalculator("David", 250);
+            ScoreCalculator("Martin", 315);
+            ScoreCalculator("Brad", 325);
 
-        public static void GreetFriend(string name)
+        }
+        public static void ScoreCalculator(string playerName, int score)
         {
-            Console.WriteLine("Hi " + name + ", my firend!");
-      
+            if( score > highscore){
+                highscore = score;
+                highScorePlayer = playerName;
+                Console.WriteLine("New High Score is " + score);
+                Console.WriteLine("High Score Holder is: " +playerName);
+            }else
+            {
+                Console.WriteLine("The old score stands, please try again. High score is still " +highscore+ " and held by " +highScorePlayer);
+            }
         }
     }
 }
